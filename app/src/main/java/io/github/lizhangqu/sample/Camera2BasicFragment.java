7fdda23609166c450c71ebae7ede055a44c11112
Mary <maryana.kravtsova@vakoms.com.ua>
2018-11-06 01:36:45 +0200

    empty line removed

--- app/src/main/java/io/github/lizhangqu/sample/Camera2BasicFragment.java
+++ app/src/main/java/io/github/lizhangqu/sample/Camera2BasicFragment.java
@@ -58,7 +58,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
